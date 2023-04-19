0e2236226457dee459f6111648b0fc95a5cf03bd
Kate Tokarenko <trinatiani@yandex.ru>
2023-04-19 11:42:42 +0300

    8.3 Добавлен компонент MainEmpty Когда предложения по аренде отсутствуют, компонент MainEmpty рендерится на главной странице. В списке отображается надпись «No places to stay available», а вместо карты отображается статичное изображение.

--- project/src/pages/main-screen/main-screen.tsx
+++ project/src/pages/main-screen/main-screen.tsx
@@ -9,6 +9,7 @@ import Map from '../../components/map/Map';
 import CitiesList from '../../components/cities-list/CitiesList';
 import Header from '../../components/header/Header';
 import OptionsList from '../../components/options-list/OptionsList';
+import MainEmpty from '../../components/main-empty/MainEmpty';
 
 function MainScreen(): JSX.Element {
   const [selectedPoint, setSelectedPoint] = useState<Offer | undefined>(
@@ -76,36 +77,39 @@ function MainScreen(): JSX.Element {
             <CitiesList cities={cities} />
           </section>
         </div>
-        <div className="cities">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">
-                {`${currentOffers.length} ${
-                  currentOffers.length === 1 ? 'place' : 'places'
-                } to stay in ${city}`}
-              </b>
-              {<OptionsList />}
-              {
-                <PlacesList
-                  offers={currentOffers}
-                  onListItemHover={onListItemHover}
-                />
-              }
-            </section>
-            <div className="cities__right-section">
-              <section className="cities__map map">
-                {currentOffers.length >= 1 && (
-                  <Map
-                    city={currentOffers[0].city}
+        {currentOffers.length ? (
+          <div className="cities">
+            <div className="cities__places-container container">
+              <section className="cities__places places">
+                <h2 className="visually-hidden">Places</h2>
+                <b className="places__found">
+                  {`${currentOffers.length} ${
+                    currentOffers.length === 1 ? 'place' : 'places'
+                  } to stay in ${city}`}
+                </b>
+                {<OptionsList />}
+                {
+                  <PlacesList
                     offers={currentOffers}
-                    selectedPoint={selectedPoint}
+                    onListItemHover={onListItemHover}
                   />
-                )}
+                }
               </section>
+              <div className="cities__right-section">
+                <section className="cities__map map">
+                  {currentOffers.length >= 1 && (
+                    <Map
+                      city={currentOffers[0].city}
+                      offers={currentOffers}
+                      selectedPoint={selectedPoint}
+                    />
+                  )}
+                </section>
+              </div>
             </div>
-          </div>
-        </div>
+          </div>) : (
+          <MainEmpty />
+        )}
       </main>
     </div>
   );
